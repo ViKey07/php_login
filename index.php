62f729a0810c7a7d615d252076f2f4c3a70693b4
Vedant <ved.k96@gmail.com>
2023-05-25 15:08:41 +0530

    completed

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="icon" href="./logo-t-b.png">
     <link rel="stylesheet" href="./partials/index.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
